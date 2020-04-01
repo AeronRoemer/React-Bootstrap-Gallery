98bfc362b3ff8ee70f8d51dd23cec62e373898c3
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-04-01 16:01:52 -0600

    Components broken out in Files

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
 import React from 'react';
+
+//Component and variable imports
+import apiKey from './config.js';
+import SearchForm from './SearchForm';
+import NavBar from './NavBar';
+import PhotoResults from './PhotoResults';
+
+class App extends React.Component {
+    render(){
+        return (
+    <div className="app">
+        <SearchForm />
+        <NavBar />
+        <PhotoResults />
+    </div>
+    );
+  }
+}
+
+export default App;
