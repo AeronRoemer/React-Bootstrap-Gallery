815fa4ccd39d1f7b2f6bcd2182a8db34ae59d46b
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-04-01 19:46:28 -0600

    Axios Flicker call

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 //Component and variable imports
 import apiKey from './config.js';
@@ -7,6 +8,23 @@ import NavBar from './NavBar';
 import PhotoResults from './PhotoResults';
 
 class App extends React.Component {
+    state ={
+        photos: ''
+    }
+
+    componentDidMount(){
+     axios.get(
+         `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=sunsets&per_page=25&format=json&nojsoncallback=1`)
+        .then(response => {
+
+            this.setState({photos: response.data.photos.photo
+            })
+           })
+         .catch(error => {
+          console.log('Error fetching and parsing data', error);
+         });
+
+    }
     render(){
         return (
     <div className="app">
